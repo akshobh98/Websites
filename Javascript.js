615f11dbe486d4c1cf2b0c9fa872d8b602a505ee
Akshobh Mirapurkar <akshobhm04@gmail.com>
2017-12-24 02:45:32 -0800

    Add files via upload

---
+++ Javascript.js
@@ -0,0 +1,37 @@
+			function change(){
+				alert("Check the album out now!");
+			}
+
+			function terms(){
+				alert("By submitting my information above, I acknowledge that I have reviewed and agreed to the PRIVACY POLICY and TERMS OF USE, and I agree to receive updates and marketing messages from time to time from Mac miller and his record label.")
+			}
+
+			function dang(){
+				var flip = document.getElementById('dang');
+				flip.src = "dangHoodie1.png";
+			}
+
+			function miller(){
+				document.getElementById('dang').src = "dangHoodie.png"
+			}
+
+			function pink(){
+				document.getElementById('tshirt').src = "PinkDangShirt1.png"
+			}
+
+			function sink(){
+				document.getElementById('tshirt').src = "PinkDangShirt.png"
+			}
+
+			function hat(){
+				document.getElementById('hat').src = "PinkHat1.png"
+			}
+
+			function hats(){
+				document.getElementById('hat').src = "PinkHat.png"
+			}
+
+			function toggle(){
+				document.getElementById('menu').classList.toggle('menu');
+				
+			}
